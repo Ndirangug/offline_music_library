f9644d6a9e70a715e330ef711f3b570cf9287da2
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-08 18:46:38 +0300

    Added bare function definitions for the MusicFilesLoader class
    Signed-off-by: George Ndirangu <ndirangu.mepawa@gmail.com>

---
+++ src/fileSystemOps/MusicFilesLoader.kt
@@ -0,0 +1,26 @@
+package offlineMusicLibrary.fileSystemOps
+
+import kotlin.test.todo
+
+class MusicFilesLoader(var location: String) {
+
+    fun loadFilesToBeProcessed(): List<MusicFile>  {
+        TODO("implement loadFilesToBeProcessed")
+        
+        return emptyList()
+        
+    }
+    
+    fun fetchListOfFilePaths(): List<String>{
+        TODO("implement fetchListOfFilePaths")
+
+
+        return emptyList()
+    }
+
+    fun createMusicFileObjects(): List<MusicFile> {
+        TODO("implement createMusicFileObjects")
+
+        return emptyList()
+    }
+}
