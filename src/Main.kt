92111ccfcd99224d8d14f179a5235a4d3663bff1
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-09 19:46:17 +0300

    implemented fetchListOfFilePaths.  filtering filetypes not yet fully implemented
    Signed-off-by: George Ndirangu <ndirangu.mepawa@gmail.com>

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,16 @@
 package offlineMusicLibrary
 
+import offlineMusicLibrary.fileSystemOps.*
+
 /**
  * Entry point. No implementations here
  *
 */
 fun main() {
+    val folderWithFiles = "/media/georgen/LOCAL DISK/George_Ndirangu/Music/"
+    val allowableFileTypes = hashSetOf(FileTypes.MP3, FileTypes.OGG)
 
+    val musicFilesLoader = MusicFilesLoader(folderWithFiles, allowableFileTypes)
+
+    musicFilesLoader.loadFilesToBeProcessed()
 }
--- src/fileSystemOps/MusicFilesLoader.kt
+++ src/fileSystemOps/MusicFilesLoader.kt
@@ -1,26 +1,63 @@
 package offlineMusicLibrary.fileSystemOps
 
-import kotlin.test.todo
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.streams.toList
 
-class MusicFilesLoader(var location: String) {
+enum class FileTypes{
+    MP3, OGG, WAV, ACC
+}
+
+class MusicFilesLoader(var location: String, var allowableFileTypes: HashSet<FileTypes>) {
+
+    private val listOfFilePaths = mutableListOf<String>()
+
 
     fun loadFilesToBeProcessed(): List<MusicFile>  {
-        TODO("implement loadFilesToBeProcessed")
-        
+        //TODO implement loadFilesToBeProcessed
+
+        val listOfFiles = fetchListOfFilePaths()
+        val unsupportedAndSupportedMap = filterFilesAccordingToSupportedAudioFormats(listOfFiles)
+        println(unsupportedAndSupportedMap.toString())
         return emptyList()
         
     }
-    
-    fun fetchListOfFilePaths(): List<String>{
-        TODO("implement fetchListOfFilePaths")
 
 
-        return emptyList()
+    fun fetchListOfFilePaths(): List<Path> {
+
+        val directoryPath = Paths.get(this.location)
+
+        return Files.walk(directoryPath)
+            .filter { item -> Files.isRegularFile(item) }
+            .toList()
     }
 
-    fun createMusicFileObjects(): List<MusicFile> {
-        TODO("implement createMusicFileObjects")
+    fun filterFilesAccordingToSupportedAudioFormats(filePathsList: List<Path>): Map<String, List<Path>>{
+        //TODO confirm filetypes that support audio tagging and filter
+        val filesWithSupportedFormat = mutableListOf<Path>()
+        val filesWithUnSupportedFormat = mutableListOf<Path>()
 
-        return emptyList()
+        filePathsList.forEach { item ->
+            run {
+                if (item.toString().endsWith("mp3")) { //TODO implement regexp
+                    filesWithSupportedFormat.add(item)
+                } else {
+                    filesWithUnSupportedFormat.add(item)
+                }
+            }
+        }
+
+        return mapOf("SUPPORTED" to filesWithSupportedFormat, "UNSUPPORTED" to filesWithUnSupportedFormat)
+    }
+
+    fun createMusicFileObjects(): MutableList<MusicFile> {
+        val musicList = mutableListOf<MusicFile>()
+
+        for (path: String in listOfFilePaths){
+            //TODO implement createMusicFileObjects
+        }
+        return musicList
     }
 }
