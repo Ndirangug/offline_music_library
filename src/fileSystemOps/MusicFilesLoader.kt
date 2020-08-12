ef05a6e3f9ee5660c26c03c1e092908ca72064f6
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-12 05:38:09 +0300

    implemented tag reading with jaudiotagger ..pending code review..also creating musicfile objects from list of files

--- src/fileSystemOps/MusicFilesLoader.kt
+++ src/fileSystemOps/MusicFilesLoader.kt
@@ -1,36 +1,39 @@
 package offlineMusicLibrary.fileSystemOps
 
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.tag.FieldKey
+import org.jaudiotagger.tag.Tag
+import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.time.Year
 import kotlin.streams.toList
 
+
 enum class FileTypes {
     MP3, OGG, WAV, ACC, MP4, M4A, M4P, WMA, FLAC, AIFF, DSF
 }
 
-class MusicFilesLoader(var location: String, var allowableFileTypes: HashSet<FileTypes>) {
-
-    private lateinit var listOfFilePathsToBeProcessed: MutableList<Path>
+class MusicFilesLoader(private var location: String, private var allowableFileTypes: HashSet<FileTypes>) {
 
 
-    fun getMusicFilesToBeProcessed(): MutableList<MusicFile> {
-        populateListOfFilePathsToBeProcessed()
+    private lateinit var mapOfUnsupportedAndSupportedFiles: Map<String, List<Path>>
 
-        return createMusicFileObjectsFromListOfFilePathsToBeProcessed()
+    fun getMusicFilesToBeProcessed(): MutableList<Path> {
+        populatemapOfUnsupportedAndSupportedFiles()
+
+        return mapOfUnsupportedAndSupportedFiles["FILES_WITH_SUPPORTED_FILE_FORMAT"] as MutableList<Path>
 
     }
 
-    private fun populateListOfFilePathsToBeProcessed() {
+    private fun populatemapOfUnsupportedAndSupportedFiles() {
         //TODO implement loadFilesToBeProcessed
         // for those in filesWithUnSupportedFormat, give optins to handle them if they're valid audio files, else discard, maybe with warning
 
         val listOfFiles = fetchListOfFilePaths()
-        val unsupportedAndSupportedMap = filterFilesAccordingToSupportedAudioFormats(listOfFiles)
-        listOfFilePathsToBeProcessed = unsupportedAndSupportedMap["FILES_WITH_SUPPORTED_FILE_FORMAT"] as MutableList<Path>
 
-        println(unsupportedAndSupportedMap.toString())
+        mapOfUnsupportedAndSupportedFiles = filterFilesAccordingToSupportedAudioFormats(listOfFiles)
 
 
     }
@@ -76,33 +79,5 @@ class MusicFilesLoader(var location: String, var allowableFileTypes: HashSet<Fil
         )
     }
 
-    private fun createMusicFileObjectsFromListOfFilePathsToBeProcessed(): MutableList<MusicFile> {
-        val musicList = mutableListOf<MusicFile>()
 
-        for (path: Path in listOfFilePathsToBeProcessed) {
-            //TODO implement createMusicFileObjects
-            val tags = readID3TagsFromFilePath(path)
-
-            musicList.add(
-                MusicFile(
-                    path,
-                    tags.getOrDefault("title", ""),
-                    tags.getOrDefault("album", ""),
-                    tags.getOrDefault("albumArtist", ""),
-                    listOf(tags.getOrDefault("contributingArtists", "")),
-                    tags.getOrDefault("genre", ""),
-                    Year.parse(tags.getOrDefault("year", "")),
-                    tags.getOrDefault("trackNumber", "").toInt()
-                )
-            )
-        }
-
-        return musicList
-    }
-
-    private fun readID3TagsFromFilePath(filePath: Path): HashMap<String, String> {
-        //TODO implement createMusicFileObjects
-
-        return hashMapOf()
-    }
 }
---
+++ src/tagging/TagReader.kt
@@ -0,0 +1,109 @@
+package offlineMusicLibrary.tagging
+
+import offlineMusicLibrary.fileSystemOps.MusicFile
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.tag.FieldKey
+import org.jaudiotagger.tag.Tag
+import java.io.File
+import java.io.FileNotFoundException
+import java.nio.file.Path
+import java.time.Year
+
+class TagReader(private var listOfFilePathsToBeProcessed: MutableList<Path>) {
+
+    fun createMusicFileObjectsFromListOfFilePathsToBeProcessed(): MutableList<MusicFile> {
+        val musicList = mutableListOf<MusicFile>()
+
+        for (path: Path in listOfFilePathsToBeProcessed) {
+
+            val tags = readID3TagsFromFilePath(path)
+
+
+            musicList.add(
+                MusicFile(
+                    path,
+                    tags.getOrDefault("title", ""),
+                    tags.getOrDefault("album", ""),
+                    tags.getOrDefault("albumArtist", ""),
+                    tags.getOrDefault("contributingArtists", "").split('/'),
+                    tags.getOrDefault("genre", ""),
+                    Year.parse(tags.getOrDefault("year", "0000").split('-')[0]),
+                    tags.getOrDefault("trackNumber", "0").toInt()
+                )
+            )
+        }
+
+        return musicList
+    }
+
+    private fun readID3TagsFromFilePath(filePath: Path): HashMap<String, String> {
+
+        val fileHandle = File(filePath.toString())
+        val hashMapOfTags: HashMap<String, String>
+
+        if (!fileHandle.isFile) {
+            throw FileNotFoundException("Couldn't find file $filePath .Please check that the name is correct")
+            //TODO maybe handle this error better
+        }
+
+        return try {
+            attemptToCreateHashMapFromExistingTags(fileHandle)
+        } catch (e: org.jaudiotagger.audio.exceptions.CannotReadException) {
+            hashMapOfTags = createHashMapOfEmptyTags()
+            validateHashMapOfTags(hashMapOfTags)
+            hashMapOfTags
+        }
+
+    }
+
+    private fun attemptToCreateHashMapFromExistingTags(fileHandle: File): HashMap<String, String> {
+        val hashMap: HashMap<String, String>
+        val f = AudioFileIO.read(fileHandle)
+        val tag: Tag? = f.tag
+
+        hashMap = if (tag != null) {
+            hashMapOf(
+                "title" to tag.getFirst(FieldKey.TITLE),
+                "album" to tag.getFirst(FieldKey.ALBUM),
+                "albumArtist" to tag.getFirst(FieldKey.ALBUM_ARTIST),
+                "contributingArtists" to tag.getFirst(FieldKey.ARTISTS),
+                "genre" to tag.getFirst(FieldKey.GENRE),
+                "year" to tag.getFirst(FieldKey.YEAR),
+                "trackNumber" to tag.getFirst(FieldKey.TRACK)
+            )
+        } else {
+            createHashMapOfEmptyTags()
+        }
+
+        validateHashMapOfTags(hashMap)
+        return hashMap
+    }
+
+    private fun createHashMapOfEmptyTags(): HashMap<String, String> {
+        return hashMapOf(
+            "title" to "",
+            "album" to "",
+            "albumArtist" to "",
+            "contributingArtists" to "",
+            "genre" to "",
+            "year" to "0000",
+            "trackNumber" to "0"
+        )
+    }
+
+    private fun validateHashMapOfTags(hashMap: HashMap<String, String>) {
+
+        hashMap.forEach { (key, value) ->
+            run {
+                if (key == "year" && value.length < 4) {
+                    hashMap[key] = "0000"
+                } else if (key == "trackNumber" && value == "") {
+                    hashMap[key] = "0"
+                }
+
+            }
+        }
+    }
+
+
+}
