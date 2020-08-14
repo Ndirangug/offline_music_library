4c43f222b7b8afb987435764074b8034eefae80d
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-14 22:51:48 +0300

    changed method signature assertListEqual() to use generics

--- src/test/assertions/IMusicListAssertions.kt
+++ src/test/assertions/IMusicListAssertions.kt
@@ -4,10 +4,12 @@ import offlineMusicLibrary.fileSystemOps.MusicFile
 import org.junit.jupiter.api.Assertions.assertEquals
 
 interface IMusicListAssertions {
-    fun assertListEqual(actual: MutableList<MusicFile>, expected: MutableList<MusicFile>) {
+    fun <T> assertListEqual(actual: List<T>, expected: List<T>) {
         for (i in actual.indices) {
             assertEquals(expected[i].toString(), actual[i].toString())
             //TODO  maybe find a more effecient way for this
         }
     }
+
+
 }
