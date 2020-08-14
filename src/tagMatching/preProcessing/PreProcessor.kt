8246dca40428c185334d83378b471fc6659c4642
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-15 02:45:46 +0300

    implemented method breakFileNameIntoChunks() added method tidyUpFileName() added method splitIntoAllPossibleSubstrings()

--- src/tagMatching/preProcessing/PreProcessor.kt
+++ src/tagMatching/preProcessing/PreProcessor.kt
@@ -4,7 +4,7 @@ import offlineMusicLibrary.fileSystemOps.MusicFile
 
 class PreProcessor() {
 
-    companion object{
+    companion object {
         fun sortLeastTaggedToMostTagged(musicList: MutableList<MusicFile>): MutableList<MusicFile> {
             musicList.sortByDescending { it.numberOfEmptyFields }
 
@@ -12,8 +12,45 @@ class PreProcessor() {
         }
 
         fun breakFileNameIntoChunks(fileName: String): List<String> {
-            //TODO implement this
-            return emptyList()
+
+            var fileName = tidyUpFileName(fileName)
+            val initialChunksFromFileName = fileName.split(" ")
+            var numberOfWords = initialChunksFromFileName.lastIndex + 1
+
+            return splitIntoAllPossibleSubstrings(initialChunksFromFileName, numberOfWords)
+
+        }
+
+        private fun tidyUpFileName(fileName: String): String {
+            var fileName = fileName.substring(0, fileName.length - 3)  //cut out last three chars, safe to assume its a filename extension
+            fileName = fileName.replace(Regex("[^A-Za-z]"), " ") //remove all non-alphabetical character
+            fileName = fileName.replace(Regex("\\s{1,5}"), " ") // remove consecutive spaces
+            fileName = fileName.trim() // remove trailing white space
+            fileName = fileName.toLowerCase()
+            return fileName
+        }
+
+        private fun splitIntoAllPossibleSubstrings(fileNameArray: List<String>, numberOfWords: Int): MutableList<String> {
+            val listOfChunks = mutableListOf<String>()
+            var chunkBuffer = ""
+            // Pick starting point
+            for (len in 1..numberOfWords) {
+
+                // Pick ending point
+                for (i in 0..numberOfWords - len) {
+                    //  Print words from current  starting point to current ending point.
+                    val j = i + len - 1
+                    for (k in i..j) {
+                        chunkBuffer += "${fileNameArray[k]} "
+
+                    }
+                    chunkBuffer = chunkBuffer.trim()
+                    listOfChunks.add(chunkBuffer)
+                    chunkBuffer = ""
+                }
+            }
+
+            return listOfChunks
         }
     }
 
