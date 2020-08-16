d335f22752e4354ab6e3ac057f67723209fb5f0b
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-16 18:24:01 +0300

    added unimplemented test MusicbrainzSearchTest

---
+++ src/test/onlineTagMatching/onlineDatabasesSearching/MusicbrainzSearchTest.kt
@@ -0,0 +1,12 @@
+package onlineTagMatching.onlineDatabasesSearching
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MusicbrainzSearchTest {
+
+    @Test
+    fun attemptRetrieveMetaTags() {
+    }
+}
