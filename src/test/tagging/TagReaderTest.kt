857bccbb41a112ba53caf2ca7462db027f8cc381
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-17 15:04:45 +0300

    moved test function createMusicFileObjectsFromListOfFilePathsToBeProcessed() MusicFileCreatorTest

--- src/test/tagging/TagReaderTest.kt
+++ src/test/tagging/TagReaderTest.kt
@@ -1,89 +1,86 @@
 package tagging
 
 import assertions.IMusicListAssertions
-import offlineMusicLibrary.fileSystemOps.FileTypes
-import offlineMusicLibrary.fileSystemOps.MusicFile
-import offlineMusicLibrary.fileSystemOps.MusicFilesLoader
 import offlineMusicLibrary.tagging.TagReader
 import org.jaudiotagger.audio.AudioFileIO
-import org.jaudiotagger.tag.Tag
 import org.junit.jupiter.api.Test
 import java.io.File
-import java.nio.file.Path
 import java.nio.file.Paths
-import java.time.Year
-import java.util.*
 import kotlin.test.assertEquals
 
 internal class TagReaderTest : IMusicListAssertions {
 
-
-
-
-    private val folderWithFiles =  Paths.get("test_assets/").toAbsolutePath().toString()
-    private lateinit var allowableFileTypes: HashSet<FileTypes>
-    private lateinit var musicFilesLoader: MusicFilesLoader
-    private lateinit var listOfFilePathsToBeProcessed: MutableList<Path>
-
-
     @Test
-    fun createMusicFileObjectsFromListOfFilePathsToBeProcessed() {
-        setUp()
+    fun readingTagsFromTaggedFileReturnsHashMapWithCorrectValues(){
+       val filePath = Paths.get("test_assets/14 Bethel Music, Bethany Wohrle & Dante Bowe - Prepare The Way.mp3").toAbsolutePath()
 
-        // existing tags are appropriately populated
-        // absent ones are replaced with the predefined default values in TagReader.createHashMapOfEmptyTags()
-        val expected = mutableListOf(
-            MusicFile(
-                filePath = Paths.get("test_assets/(10) Man of Your Word (feat. Chandler Moore & KJ Scriven) - Maverick City _ TRIBL - YouTube-converted.mp3").toAbsolutePath(),
-                title = "",
-                album = "",
-                albumArtist = "",
-                contributingArtists = listOf<String>(),
-                genre = "",
-                year = Year.parse("0000"),
-                trackNumber = 0,
-                trackLength = "546",
-                numberOfEmptyFields = 7
-            ),
-            MusicFile(
-                filePath = Paths.get("test_assets/14 Bethel Music, Bethany Wohrle & Dante Bowe - Prepare The Way.mp3").toAbsolutePath(),
-                title = "Prepare The Way",
-                album = "Revival's In The Air",
-                albumArtist = "Bethel Music",
-                contributingArtists = listOf<String>("Bethel Music", "Bethany Wohrle", "Dante Bowe" ),
-                genre = "",
-                year = Year.parse("2020"),
-                trackNumber = 14,
-                trackLength = "321",
-                numberOfEmptyFields = 1
-            ),
-            MusicFile(
-                filePath = Paths.get("test_assets/Cory_Asbury_Endless_Alleluia.hd.ogg").toAbsolutePath(),
-                title = "",
-                album = "",
-                albumArtist = "",
-                contributingArtists = listOf<String>(),
-                genre = "",
-                year = Year.parse("0000"),
-                trackNumber = 0,
-                trackLength = "",
-                numberOfEmptyFields = 8
-            )
+        val expected =  hashMapOf(
+            "title" to "Prepare The Way",
+            "album" to "Revival's In The Air",
+            "albumArtist" to "Bethel Music",
+            "contributingArtists" to "Bethel Music/Bethany Wohrle/Dante Bowe",
+            "genre" to "",
+            "year" to "2020-05-29",
+            "trackNumber" to 14,
+            "trackLength" to "321",
+            "numberOfEmptyFields" to 1
         )
 
-        val actual = TagReader(listOfFilePathsToBeProcessed).createMusicFileObjectsFromListOfFilePathsToBeProcessed()
+        val actual = TagReader().readID3TagsFromFilePath(filePath)
 
-        assertListEqual(expected, actual)
+        assertMapEqual(expected, actual)
 
-    }
-
-    private fun setUp(){
-        allowableFileTypes = hashSetOf(FileTypes.MP3, FileTypes.OGG)
-        musicFilesLoader = MusicFilesLoader(folderWithFiles, allowableFileTypes)
-        listOfFilePathsToBeProcessed = musicFilesLoader.getMusicFilesToBeProcessed()
     }
 
     @Test
+    fun readingTagsFromUnreadableFileReturnsEmptyHashMap(){
+        val filePath = Paths.get("test_assets/Cory_Asbury_Endless_Alleluia.hd.ogg").toAbsolutePath()
+
+        val expected =  hashMapOf(
+            "title" to "",
+            "album" to "",
+            "albumArtist" to "",
+            "contributingArtists" to "",
+            "genre" to "",
+            "year" to "0000",
+            "trackNumber" to "0",
+            "trackLength" to "",
+            "numberOfEmptyFields" to "8"
+        )
+
+        val actual = TagReader().readID3TagsFromFilePath(filePath)
+
+
+        assertMapEqual(expected, actual)
+
+
+    }
+
+    @Test
+    fun readingTagsFromUntaggedFileReturnsHashMapWithOnlyTrackLenth(){
+        val filePath = Paths.get("test_assets/(10) Man of Your Word (feat. Chandler Moore & KJ Scriven) - Maverick City _ TRIBL - YouTube-converted.mp3").toAbsolutePath()
+        val expected =  hashMapOf(
+            "title" to "",
+            "album" to "",
+            "albumArtist" to "",
+            "contributingArtists" to "",
+            "genre" to "",
+            "year" to "0000",
+            "trackNumber" to "0",
+            "trackLength" to "546",
+            "numberOfEmptyFields" to "7"
+        )
+
+        val actual = TagReader().readID3TagsFromFilePath(filePath)
+
+        assertMapEqual(expected, actual)
+
+    }
+
+
+
+
+    @Test       // learning test
     fun tryToReadEmptyAudioHeaderReturnsEmptyString(){
         val filePath = Paths.get("test_assets/Cory_Asbury_Endless_Alleluia.hd.ogg").toAbsolutePath()
         val fileHandle = File(filePath.toString())
@@ -102,7 +99,7 @@ internal class TagReaderTest : IMusicListAssertions {
         assertEquals(expectedTrackLength, actualTrackLength)
     }
 
-    @Test
+    @Test   // learning test
     fun tryToReadAudioHeaderTrackLenthReturnsSeconds(){
         val filePath = Paths.get("test_assets/(10) Man of Your Word (feat. Chandler Moore & KJ Scriven) - Maverick City _ TRIBL - YouTube-converted.mp3").toAbsolutePath()
         val fileHandle = File(filePath.toString())
