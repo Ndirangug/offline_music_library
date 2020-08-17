f7d1581aaef065a2b6c702859c4dfec96b73c22b
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-17 15:09:57 +0300

    started implementing  MusicbrainzSearch
    added overrides for methods inherited from interface DatabaseSearch

--- src/onlineTagMatching/onlineDatabasesSearching/MusicbrainzSearch.kt
+++ src/onlineTagMatching/onlineDatabasesSearching/MusicbrainzSearch.kt
@@ -5,7 +5,21 @@ import offlineMusicLibrary.fileSystemOps.MusicFile
 class MusicbrainzSearch{
 
     companion object: DatabaseSearch{
+        override fun attemptRetrieveMetaTags(musicFile: MusicFile): MusicFile {
+            //TODO implement this
+            return super.attemptRetrieveMetaTags(musicFile)
+        }
 
+        override fun search(musicFile: MusicFile): HashMap<String, String> {
+            //TODO implement this
+            return super.search(musicFile)
+        }
+
+        override fun updateMusicFile(musicFile: MusicFile, searchResults: HashMap<String, String>) {
+            //TODO implement this
+            super.updateMusicFile(musicFile, searchResults)
+
+        }
     }
 
 
