bd1a59eec0f6ccc259536108081aba70389a7fd7
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-28 17:57:38 +0300

    cleaned up  companion object to only  override  search(), searchUsingFileNameFragments() from IDatabaseSearch
    Signed-off-by: George Ndirangu <ndirangu.mepawa@gmail.com>

--- src/onlineTagMatching/onlineDatabasesSearching/MusicbrainzSearch.kt
+++ src/onlineTagMatching/onlineDatabasesSearching/MusicbrainzSearch.kt
@@ -1,24 +1,40 @@
 package offlineMusicLibrary.onlineTagMatching.onlineDatabasesSearching
 
 import offlineMusicLibrary.fileSystemOps.MusicFile
+import offlineMusicLibrary.onlineTagMatching.preProcessing.PreProcessor
 
 class MusicbrainzSearch{
 
     companion object: IDatabaseSearch{
-        override fun attemptRetrieveMetaTags(musicFile: MusicFile): MusicFile {
-            //TODO implement this
-            return super.attemptRetrieveMetaTags(musicFile)
-        }
+
+        //only override methods search() and searchUsingFileNameFragments() to suite this specific use case.
+        //Only these two need to be customized.The rest of the methods, from IDatabaseSearch, the inherited implementations already work perfectly
+        //for all use cases
 
         override fun search(musicFile: MusicFile): HashMap<String, String> {
             //TODO implement this
-            return super.search(musicFile)
+            //first try fileNameFragments after determining that its not random sequence
+            //TODO if not work fallback to pre existing tags
+            val fileNameFragments = PreProcessor.breakFileNameIntoChunks(musicFile.filePath.fileName.toString())
+            return searchUsingFileNameFragments(fileNameFragments)
+
         }
 
-        override fun updateMusicFile(musicFile: MusicFile, searchResults: HashMap<String, String>) {
-            //TODO implement this
-            super.updateMusicFile(musicFile, searchResults)
 
+        override fun searchUsingFileNameFragments(fragments: List<String>): HashMap<String, String> {
+            //TODO implement this
+            // search musicbrainz here! heres the spot
+            return hashMapOf(
+                "title" to "",
+                "album" to "",
+                "albumArtist" to "",
+                "contributingArtists" to "",
+                "genre" to "",
+                "year" to "0000",
+                "trackNumber" to "0",
+                "trackLength" to "",
+                "numberOfEmptyFields" to "8"
+            )
         }
     }
 
