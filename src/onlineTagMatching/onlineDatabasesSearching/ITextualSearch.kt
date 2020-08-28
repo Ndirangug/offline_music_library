a7e2999c834b31310cc25493b6ee68283ba61079
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-08-28 17:53:03 +0300

    added function searchUsingFileNameFragments()
    Signed-off-by: George Ndirangu <ndirangu.mepawa@gmail.com>

--- src/onlineTagMatching/onlineDatabasesSearching/ITextualSearch.kt
+++ src/onlineTagMatching/onlineDatabasesSearching/ITextualSearch.kt
@@ -1,4 +1,8 @@
 package offlineMusicLibrary.onlineTagMatching.onlineDatabasesSearching
 
 interface ITextualSearch {
+
+    fun searchUsingFileNameFragments(fragment: List<String>): HashMap<String, String> {
+            return hashMapOf()
+    }
 }
